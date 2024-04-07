7a829d29b7a8d27c02969c4d69a0ece03c1eb0bb
Geoffrey Weal <geoffreyweal@Geoffreys-iMac.local>
2024-04-07 16:28:30 +0900

    updated PackwoodGPU Instructions

--- pushing_to_github.sh
+++ pushing_to_github.sh
@@ -1,12 +1,17 @@
+# ------------------------------------------
 # For commiting to github for the first time
-rm -rf .git
-git init
+#rm -rf .git
+#git init
+# ------------------------------------------
 # upload to github
 git add .
 git commit -m 'updated PackwoodGPU Instructions'
+# ------------------------------------------
 # For commiting to github for the first time
-git branch -M main
-git remote add origin git@github.com:geoffreyweal/PackwoodGPU-Instructions.git
-git push -uf origin main
+#git branch -M main
+#git remote add origin git@github.com:geoffreyweal/PackwoodGPU-Instructions.git
+#git push -uf origin main
+# ------------------------------------------
 # push your new commit:
 git push -u origin main
+# ------------------------------------------
